14e410f73cb29fe27c28f1eb9ae31903beafaae0
fred <frederic-guerra_student2021@wild.school>
2021-04-17 16:19:01 +0200

    ajout twig et création page home

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,16 +2,11 @@
 
 namespace App\Controller;
 
-use App\Taxes\Calculator;
-use App\Taxes\Detector;
-use Cocur\Slugify\Slugify;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
-class HelloController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
 {
     //protected $calculator;
     //protected $logger;
@@ -21,19 +16,12 @@ class HelloController
     // }
 
     /**
-     * @Route("/hello/{nom?world}", name="hello")
+     * @Route("/hello/{nom?Fred}", name="hello")
      */
-    public function helloWorld($nom = "world", Environment $twig)
+    public function helloWorld($nom = "world")
     {
-        $html = $twig->render('hello.html.twig', [
-            'prenom' => $nom,
-            'ages' => [
-                12,
-                18,
-                29,
-                15
-            ]
+        return $this->render('hello.html.twig', [
+            'prenom' => $nom
         ]);
-        return new Response($html);
     }
 }
