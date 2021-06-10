be12af651273202cefb2b5b7b63c74b5cb489fa3
fred-fittywebdev <frederic-guerra_student2021@wilder.school>
2021-06-10 15:30:45 +0200

    Finalisation des validations, mise en place de la navbar avec les liens

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -27,7 +27,7 @@ class CategoryController extends AbstractController
         $form = $this->createForm(CategoryType::class, $category);
 
         $form->handleRequest($request);
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $category->setSlug(strtolower($slugger->slug($category->getName())));
 
             $em->persist($category);
@@ -58,7 +58,7 @@ class CategoryController extends AbstractController
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $em->flush();
 
             return $this->redirectToRoute('homepage');
