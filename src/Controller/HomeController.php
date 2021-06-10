2cfcc848dd35403fc6f97124deec05659c0ea6d5
fred-fittywebdev <frederic-guerra_student2021@wilder.school>
2021-06-10 20:45:15 +0200

    Création de la requête DQL match against, création et mise en place du formulaire dans la nav

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,21 +2,34 @@
 
 namespace App\Controller;
 
+use App\Form\SearchProductType;
 use App\Repository\ProductRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage(ProductRepository $productRepository)
+    public function homepage(ProductRepository $productRepository, Request $request)
     {
+
+
         $products = $productRepository->findBy([], [], 3);
 
+        $form = $this->createForm(SearchProductType::class);
+
+        $search = $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //on cherhce les produits
+        }
+
         return $this->render('home.html.twig', [
-            'products' => $products
+            'products' => $products,
+            'form' => $form->createView()
         ]);
     }
 }
